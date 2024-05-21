25b09ad7eaf1e456835d9fe26134ef1e88cea668
ellas <ella.yap26@ycdsbk12.ca>
2024-05-21 13:32:55 -0400

    Updated Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,26 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+  // Related arrays for the (x, y) coordinate of the snowflakes
+  float [] snowX = new float[15]; 
+  float [] snowY = new float[15]; 
+  int snowDiameter = 20; 
+
   float circleX = 50;
   float circleY = 50;
+  int playerLives = 3;
 
-  // Related arrays for the (x, y) coordinate of the snowflakes
-  float [] snowX = new float[30]; // 42
-  float [] snowY = new float[30]; // 42
-  int snowDiameter = 10;
+  // Declare blue player variables
+  boolean wPressed = false;
+  boolean aPressed = false;
+  boolean sPressed = false;
+  boolean dPressed = false;
+
+  // Declare snow speed variables
+  boolean upPressed = false;
+  boolean downPressed = false;
+
+  boolean[] ballHideStatus;
 
   public void settings() {
     size(400, 400);
@@ -15,6 +28,8 @@ public class Sketch extends PApplet {
 
   public void setup() {
     background(0);
+
+    ballHideStatus = new boolean [snowX.length];
 
     // Generate random x- and y- values for snowflakes
     for (int i = 0; i < snowX.length; i++) {
@@ -32,6 +47,21 @@ public class Sketch extends PApplet {
     bluePlayerCirle();
 
     playerLives();
+
+    if (wPressed) {
+      circleY--;
+    }
+    if (sPressed) {
+      circleY++;
+    }
+    if (aPressed) {
+      circleX--;
+    }
+    if (dPressed) {
+      circleX++;
+    }
+
+    mouseClicked();
   }
   
   // All other defined methods are written below:
@@ -43,16 +73,14 @@ public class Sketch extends PApplet {
       // If the down arrow is pressed, the snow falls faster
       // If the up arrow is pressed, the snow falls slower
       // If no arrow is pressed, the snow falls
-      if (keyPressed) {
-        if (keyCode == DOWN) {
-          snowY[i] += 8;
-        }
-        else if (keyCode == UP) {
-          snowY[i] += 2;
-        }
+      if (downPressed) {
+        snowY[i] += 7;
+      }
+      else if (upPressed) {
+        snowY[i] += 1;
       }
       else {
-        snowY[i] += 4;
+        snowY[i] += 3;
       }
 
       // Reset snowflakes
@@ -62,28 +90,84 @@ public class Sketch extends PApplet {
     }
   }
 
-  public void bluePlayerCirle() {
-    fill (214, 245, 255); // blue
-    ellipse(circleX, circleY, 50, 50);
-    if (keyPressed) {
-      if (key == 'w') {
-        circleY -= 2; 
-      }
-      else if (key == 'a') {
-        circleX -= 2;
-      }
-      else if (key == 's') {
-        circleY += 2;
-      }
-      else if (key == 'd') {
-        circleX += 2;
-      }
+  public void playerLives() {
+    for (int i = 0; i < playerLives; i++) {
+      float x = width - 50 - i * 50;
+      float y = 20;
+      fill (255);
+      rect (x, y, 30, 30);
+    }
+
+    if (playerLives < 0) {
+      background (255);
+      textSize (50);
+      fill (0);
+      textAlign (CENTER, CENTER);
+      text ("Game Over", width/2, height/2);
     }
   }
 
-  public void playerLives() {
-    square (width - 50, height - 380, 30);
-    square (width - 100, height - 380, 30);
-    square (width - 150, height - 380, 30);
+  public void bluePlayerCirle() {
+    circleX = constrain (circleX, 0, width);
+    circleY = constrain (circleY, 0, height);
+    
+    // Blue player circle
+    fill (214, 245, 255); // blue
+    ellipse(circleX, circleY, 50, 50);
+  }
+
+  public void keyPressed() {
+    if (keyCode == UP) {
+      upPressed = true;
+    }
+    else if (keyCode == DOWN) {
+      downPressed = true;
+    }
+
+    if (key == 'w' || key == 'W') {
+      wPressed = true;
+    }
+    if (key == 'a' || key == 'A') {
+      aPressed = true;
+    }
+    if (key == 's' || key == 'S') {
+      sPressed = true;
+    }
+    if (key == 'd' || key == 'D') {
+      dPressed = true;
+    }
+  }
+
+  public void keyReleased() {
+    if (keyCode == UP) {
+      upPressed = false;
+    }
+    else if (keyCode == DOWN) {
+      downPressed = false;
+    }
+
+    if (key == 'w' || key == 'W') {
+      wPressed = false;
+    }
+    else if (key == 'a' || key == 'A') {
+      aPressed = false;
+    }
+    else if (key == 's' || key == 'S') {
+      sPressed = false;
+    }
+    else if (key == 'd' || key == 'D') {
+      dPressed = false;
+    }
+  }
+
+  public void mouseClicked() {
+    // Make snowflakes disappear when they are clicked
+    float clickRadius = 10;
+    for (int i = 0; i < snowX.length; i++) {
+      float distance = dist(snowX[i], snowY[i], mouseX, mouseY);
+      if (distance < clickRadius) {
+        ballHideStatus[i] = true;
+      }
+    }
   }
 }
